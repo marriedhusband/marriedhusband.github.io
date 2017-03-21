751b41284d3892bd42677bfd57ae47409c4736fa
marriedhusband <marriedhusband@outlook.com>
2017-03-21 09:54:19 +0800

    Changes

---
+++ dodopizza/china/franchise/index2_files/pizzeriaCount.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    pizzeriaCount();
+});
+
+//счетчик на количество пиццерий
+function pizzeriaCount() {
+    $.getJSON(Url.GlobalDodoApi + '/api/v1/pizzerias/count?callback=?', function (data) {
+        $("#unitsCount").html(data.total);
+    });
+}
